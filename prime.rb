6161d18a3143071dc7f79f0a58a47232f1e698fa
lizaetillman <lizaetillman@gmail.com>
2020-06-28 13:17:27 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+    start = 2
+    if number > 1
+      range = (start..number-1).to_a
+      range.none? do |num_to_test| #none of the numbers should return true for none? to return true.
+        number % num_to_test == 0
+        end
+    else
+      false
+    end
+end
